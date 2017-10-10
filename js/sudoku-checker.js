c6f1a4a9a89e7fd80afc92433c7a24f1f4a2e6e4
Dawson Mortenson and Riki Montgomery <dawson+riki@>
2017-10-10 16:23:00 -0700

    add a column validaton function

--- js/sudoku-checker.js
+++ js/sudoku-checker.js
@@ -1,17 +1,25 @@
 export class Sudoku {
+  // Initializes every sudoku with a 9x9 matrix
   constructor() {
     this.board = new Array(9);
     for (let i = 0; i < 9; i++) {
       this.board[i] = [0,0,0,0,0,0,0,0,0];
     }
   }
-
-
-  // Initializes every sudoku with a 9x9 matrix
-  rowValidator(n, x, y) {
+  rowValidator(n,x,y) {
     console.log(this.board);
     for(let i = 0; i < 9; i++){
       if(this.board[y][i] === n && i != x){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  columnValidator(n,x,y) {
+    console.log(this.board);
+    for(let i = 0; i < 9; i++){
+      if(this.board[i][x] === n && i != y){
         return false;
       }
     }
---
+++ spec/sudoku-checker-spec.js
@@ -0,0 +1,14 @@
+import { Sudoku } from './../js/sudoku-checker.js';
+
+describe('Sudoku', function() {
+  it('validate rows', function() {
+    var calc = new Sudoku('')
+    expect().toEqual()
+  });
+
+// describe('Sudoku', function() {
+//   it('validate columns', function() {
+//     var calc = new Sudoku('')
+//     expect().toEqual()
+//   });
+});
